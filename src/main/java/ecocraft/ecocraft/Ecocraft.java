35b408d9bd667e6a0cb5daa96867c718d6c790df
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2022-08-14 14:27:49 +0200

    Added configuration file and modified regions to use it.

--- src/main/java/ecocraft/ecocraft/Ecocraft.java
+++ src/main/java/ecocraft/ecocraft/Ecocraft.java
@@ -11,24 +11,24 @@ import ecocraft.ecocraft.CustomBlocks.SolarPanelBase;
 import ecocraft.ecocraft.Handlers.*;
 
 import ecocraft.ecocraft.Pollution.Regions;
-import ecocraft.ecocraft.Utils.NightDetector;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
 public final class Ecocraft extends JavaPlugin {
-
-
     @Override
     public void onEnable() {
+        // Loading config file
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
 
+        // Initializing command executors
         Objects.requireNonNull(getCommand("solar")).setExecutor(new SolarPanelCommands());
         Objects.requireNonNull(getCommand("solarbase")).setExecutor(new SolarPanelBaseCommands());
         Objects.requireNonNull(getCommand("cable")).setExecutor(new CableCommands());
-        //TODO wartosci w pilku konfiguracyjnym
-        Regions.init(18432 , 9216,200000);
+
+        // Initialize regions using config file
+        Regions.init(getConfig());
 
         MainEventHandler.init(this);
         SolarPanel.getInstance();
@@ -39,13 +39,11 @@ public final class Ecocraft extends JavaPlugin {
         MainEventHandler.init(this);
 
         RecyclerBlock.register(this);
-
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
-
 
 }
--- src/main/java/ecocraft/ecocraft/Pollution/Regions.java
+++ src/main/java/ecocraft/ecocraft/Pollution/Regions.java
@@ -1,9 +1,10 @@
 package ecocraft.ecocraft.Pollution;
 
+import org.bukkit.configuration.file.FileConfiguration;
+
 import java.util.Optional;
 
 public class Regions {
-
     private static Regions regions;
 
     public static Double regionDim;
@@ -12,14 +13,23 @@ public class Regions {
 
     public static Integer height;
 
-    private Regions(Integer width, Integer height,Integer numberOfRegions) {
-        this.width = width;
-        this.height = height;
-         this.regionDim = Math.floor(Math.sqrt((height*width)/numberOfRegions));
+    private Regions(Integer width, Integer height, Integer numberOfRegions) {
+        Regions.width = width;
+        Regions.height = height;
+        Regions.regionDim = Math.floor(Math.sqrt((height * width) / numberOfRegions));
     }
 
     public static void init(Integer width,Integer height,Integer numberOfRegions){
         regions = new Regions(width,height,numberOfRegions);
+    }
+
+    public static void init(FileConfiguration config){
+        if (regions == null) return;
+        regions = new Regions(
+                config.getInt("worldWidth"),
+                config.getInt("worldHeight"),
+                config.getInt("numberOfRegions")
+        );
     }
 
     public static Regions getInstance(){
