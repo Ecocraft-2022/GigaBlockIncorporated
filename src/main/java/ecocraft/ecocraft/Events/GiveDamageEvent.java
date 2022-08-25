d838018c5cf0a41541f97d169162a2e36fd0dfcf
fafik <fafik@Fafik.localdomain>
2022-08-25 21:12:25 +0200

    Damage in heavily polluted areas

---
+++ src/main/java/ecocraft/ecocraft/Events/GiveDamageEvent.java
@@ -0,0 +1,29 @@
+package ecocraft.ecocraft.Events;
+
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class GiveDamageEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private Player player;
+
+    public GiveDamageEvent(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
