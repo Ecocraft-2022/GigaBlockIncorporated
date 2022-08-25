cd9b7dea8a49cf91e5642754446993ca9250ed02
fafik <fafik@Fafik.localdomain>
2022-08-25 18:11:58 +0200

    Custom blocks glitch fix

--- src/main/java/ecocraft/ecocraft/Handlers/CustomBlocksHandlers/Actions/DestroyCustomBlock.java
+++ src/main/java/ecocraft/ecocraft/Handlers/CustomBlocksHandlers/Actions/DestroyCustomBlock.java
@@ -7,6 +7,7 @@ import ecocraft.ecocraft.CustomBlocks.SolarPanelBase;
 import ecocraft.ecocraft.Utils.Util;
 import org.bukkit.Instrument;
 import org.bukkit.Material;
+import org.bukkit.Note;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -14,8 +15,13 @@ import org.bukkit.block.data.type.NoteBlock;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockEvent;
+import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DestroyCustomBlock implements Listener {
 
@@ -24,6 +30,14 @@ public class DestroyCustomBlock implements Listener {
     public DestroyCustomBlock(Plugin plugin) {
         this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+@EventHandler
+    public void blockEvent(BlockPhysicsEvent e){
+        Block block = e.getBlock();
+        plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+            List visited = new ArrayList();
+            prevent(block,visited);
+        },1);
     }
 
     @EventHandler
@@ -58,16 +72,36 @@ public class DestroyCustomBlock implements Listener {
     public void onCustomBlockPlaced(BlockPlaceEvent e){
         preventCustomBlockUpdateUpdate(e.getBlock());
     }
+
+
     private void preventCustomBlockUpdateUpdate(Block block){
         plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
-            if(block.getRelative(BlockFace.UP).getType().equals(Material.NOTE_BLOCK)){
-                NoteBlock nb = (NoteBlock) block.getRelative(BlockFace.UP).getBlockData();
-
-                nb.setInstrument(Instrument.GUITAR);
-                block.getRelative(BlockFace.UP).setBlockData(nb);
-                block.getRelative(BlockFace.UP).getState().update();
-            }
+            List visited = new ArrayList();
+            prevent(block,visited);
         },1);
     }
 
+    private void prevent(Block block, List<String> visited){
+        for(BlockFace face: Util.list){
+            Block relative = block.getRelative(face);
+            if(relative.getType().equals(Material.NOTE_BLOCK)){
+                String coordinates = cordsToString(relative);
+                if (!visited.contains(coordinates)) {
+                    NoteBlock nb =(NoteBlock) relative.getBlockData();
+                    nb.setInstrument(Instrument.GUITAR);
+                    relative.setBlockData(nb);
+                    relative.getState().update();
+                    visited.add(coordinates);
+                    prevent(relative, visited);
+                }
+            }
+        }
+    }
+    private String cordsToString(Block bl) {
+        StringBuilder str = new StringBuilder();
+        str.append(bl.getX());
+        str.append(bl.getY());
+        str.append(bl.getZ());
+        return str.toString();
+    }
 }
--- src/main/java/ecocraft/ecocraft/Handlers/CustomBlocksHandlers/MainBlockHandler.java
+++ src/main/java/ecocraft/ecocraft/Handlers/CustomBlocksHandlers/MainBlockHandler.java
@@ -70,6 +70,7 @@ public class MainBlockHandler implements Listener {
                 break;
         }
 
+
     }
 
     @EventHandler
